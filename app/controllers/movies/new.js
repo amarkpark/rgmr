b322591ebc7d01e244d857d607f671231fe6f5f4
Amanda Mark <amarkpark@gmail.com>
2017-10-15 21:58:16 -0500

    have an alert for full New form action

--- app/controllers/movies/new.js
+++ app/controllers/movies/new.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     addReview: function(){
-      var title = this.get('title');
-      alert(title);
+      let title = this.get('title');
+      let releaseYear = this.get('releaseYear');
+      let rating = this.get('rating');
+      let review = this.get('review')
+      alert(title + ' ' + releaseYear + ' ' + rating + ' ' + review);
     }
   }
 });
